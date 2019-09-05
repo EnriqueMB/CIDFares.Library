8ed544a463c4b7c9ab9542b1d3131e47541036f3
DanyFlores <lopezfloresjuandaniel123@gmail.com>
2019-09-05 11:25:49 -0500

    dataGrid

--- CIDFares.Library.Controls/Properties/Resources.Designer.cs
+++ CIDFares.Library.Controls/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace CIDFares.Library.Controls.Properties {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -134,6 +134,16 @@ namespace CIDFares.Library.Controls.Properties {
         internal static System.Drawing.Bitmap exclamaciontriangulo {
             get {
                 object obj = ResourceManager.GetObject("exclamaciontriangulo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap filtroblanco {
+            get {
+                object obj = ResourceManager.GetObject("filtroblanco", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
