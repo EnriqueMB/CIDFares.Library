c713635ebd138da937f16d79a66727535bb32fb4
joseLeyder <32853846+joseLeyder@users.noreply.github.com>
2019-09-26 23:02:07 -0500

    leyder

---
+++ CIDFares.Library.Controls/CIDHorario/Enums/DaysHour.cs
@@ -0,0 +1,13 @@
+﻿namespace CIDFares.Library.Controls.CIDHorario.Enums
+{
+    public enum DaysHour
+    {
+        Lunes = 1,
+        Martes = 2,
+        Miercoles = 3,
+        Jueves = 4,
+        Viernes = 5,
+        Sabado = 6,
+        Domingo = 7
+    }
+}
